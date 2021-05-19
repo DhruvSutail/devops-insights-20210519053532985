d96e45894535e475e7bc427d23fe8b734f875149
Dhruv Sutail <dhruvsutail18@gnu.ac.in>
2021-05-19 06:12:18 +0000

    first commit

--- src/components/Zip.js
+++ src/components/Zip.js
@@ -1,49 +1,116 @@
 import React, { useState } from 'react';
+function Zip(){
+	   <div class="container my-4">
 
-function Zip(props) {
+  <hr class="mb-5"/>
 
-    const [validationError, setValidationError] = useState(null);
+     <section id="register-form">
 
-    const validate = (event) => {
-        const zipCodePattern = /^\d{5}$/;
-        const valid = zipCodePattern.test(event.target.value);
-        if (!valid) {
-            setValidationError('* should be a 5 digit number only');
-            props.clearResponse();
-        } else {
-            setValidationError('');
-            props.onZipChange(event.target.value);
-        }
-    };
-
-    return (
-        <div className="col-sm-4">
-            <div className="row">
-                <div className="col-sm-10">
-                    <style jsx="true">{`
-                        .form-control::-webkit-input-placeholder {
-                            color: #ddd;
-                        }
-                    `}
-                    </style>
-                    <input 
-                        type="text" 
-                        className="form-control" 
-                        id="usr" 
-                        placeholder="US Zip Code (5 digit)"
-                        onKeyPress={(event) => {
-                            if (event.key === "Enter") {
-                                validate(event);
-                            }
-                        }}
-                    ></input>   
-                </div>
+          <!--Title-->
+          <h2 class="section-heading mb-4 h2">
+            Register / Sign up form
+          </h2>
+        
+          <!-- Description -->
+          <p>
+          </p>
+        
+          <!--Grid row-->
+          <div class="row">
+        
+            <!--Grid column-->
+            <div class="col-md-6 mb-4">
+        
+              <!--Title-->
+              <h3 class="secondary-heading mb-3">
+                Big Basket
+              </h3>
+        
+              <!--Section: Live preview-->
+              <section>
+        
+                <!-- Default form register -->
+                <form class="text-center border border-light p-5">
+        
+                  <p class="h4 mb-4">Sign up</p>
+        
+                  <div class="form-row mb-4">
+                    <div class="col">
+                      <!-- First name -->
+                      <input type="text" id="defaultRegisterFormFirstName" class="form-control" placeholder="Customer name">
+                    </div>
+                  </div>
+        	
+		<input type="email" id="defaultRegisterFormEmail" class="form-control mb-4" placeholder="Address">
+                  <!-- E-mail -->
+                  <input type="email" id="defaultRegisterFormEmail" class="form-control mb-4" placeholder="E-mail">
+        
+                  <!-- Password -->
+                  <input type="password" id="defaultRegisterFormPassword" class="form-control" placeholder="Password" aria-describedby="defaultRegisterFormPasswordHelpBlock">
+                  <small id="defaultRegisterFormPasswordHelpBlock" class="form-text text-muted mb-4">
+                    At least 8 characters and 1 digit
+                  </small>
+        
+                  <!-- Phone number -->
+                  <input type="text" id="defaultRegisterPhonePassword" class="form-control" placeholder="Phone number" aria-describedby="defaultRegisterFormPhoneHelpBlock">
+                  <small id="defaultRegisterFormPhoneHelpBlock" class="form-text text-muted mb-4">
+                    Optional - for two step authentication
+                  </small>
+        
+                  <!-- Newsletter -->
+                  <div class="custom-control custom-checkbox">
+                    <input type="checkbox" class="custom-control-input" id="defaultRegisterFormNewsletter">
+                    <label class="custom-control-label" for="defaultRegisterFormNewsletter">Subscribe to our newsletter</label>
+                  </div>
+        
+                  <!-- Sign up button -->
+                  <button class="btn btn-info my-4 btn-block waves-effect waves-light" type="submit">Sign in</button>
+        
+                  <!-- Social register -->
+                  <p>or sign up with:</p>
+        
+                  <a type="button" class="light-blue-text mx-2">
+                    <i class="fab fa-facebook-f"></i>
+                  </a>
+                  <a type="button" class="light-blue-text mx-2">
+                    <i class="fab fa-twitter"></i>
+                  </a>
+                  <a type="button" class="light-blue-text mx-2">
+                    <i class="fab fa-linkedin-in"></i>
+                  </a>
+                  <a type="button" class="light-blue-text mx-2">
+                    <i class="fab fa-github"></i>
+                  </a>
+        
+                  <hr>
+        
+                  <!-- Terms of service -->
+                  <p>By clicking
+                    <em>Sign up</em> you agree to our
+                    <a href="" target="_blank">terms of service</a>
+        
+                </p></form>
+                <!-- Default form register -->
+        
+              </section>
+              <!--Section: Live preview-->
+        
             </div>
-            <div className="pl-3 row">
-                <div className="text-danger small"> { validationError }</div>
+            <!--Grid column-->
+        
+            <!--Grid column-->
+            <div class="col-md-6 mb-4">
+        
+             
             </div>
-        </div>
-    );
+            <!--Grid column-->
+        
+          </div>
+          <!--Grid row-->
+        
+        </section>
+
+  </div>
 }
 
 export default Zip
